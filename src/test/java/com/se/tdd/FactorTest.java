120c193e9e45d5b23e1b43380fb6ea95a56e8498
D Dranidis <dranidis@gmail.com>
2021-04-02 22:02:17 +0300

    Factors 2: tests fail

--- src/test/java/com/se/tdd/FactorTest.java
+++ src/test/java/com/se/tdd/FactorTest.java
@@ -16,4 +16,9 @@ public class FactorTest {
         assertEquals(Arrays.asList(), Factor.getFactors(1));
     }
 
+    @Test
+    public void getFactors_of_2_returns_2() {
+        assertEquals(Arrays.asList(2), Factor.getFactors(2));
+    }
+
 }

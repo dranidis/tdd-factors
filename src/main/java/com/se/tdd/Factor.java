1d965fb97acc57d5666a7cbd74442fc6e5378949
D Dranidis <dranidis@gmail.com>
2021-04-02 23:15:03 +0300

    Factors 4: tests pass

--- src/main/java/com/se/tdd/Factor.java
+++ src/main/java/com/se/tdd/Factor.java
@@ -7,8 +7,14 @@ public class Factor {
 
     public static List<Integer> getFactors(int i) {
         List<Integer> list = new ArrayList<>();
-        if (i > 1)
-            list.add(i);
+        if (i > 1) {
+            if (i % 2 == 0) {
+                list.add(2);
+                i /= 2;
+            }
+            if (i > 1)
+                list.add(i);
+        }
         return list;
     }
 

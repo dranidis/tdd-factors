d364eb2ad19b6bd46a078d1724bf8b34bd1e7b95
D Dranidis <dranidis@gmail.com>
2021-04-02 23:21:42 +0300

    Factors 5,6,7: tests pass

--- src/test/java/com/se/tdd/FactorTest.java
+++ src/test/java/com/se/tdd/FactorTest.java
@@ -30,4 +30,20 @@ public class FactorTest {
     public void getFactors_of_4_returns_2_2() {
         assertEquals(Arrays.asList(2, 2), Factor.getFactors(4));
     }
+
+    @Test
+    public void getFactors_of_5_returns_5() {
+        assertEquals(Arrays.asList(5), Factor.getFactors(5));
+    }
+
+    @Test
+    public void getFactors_of_6_returns_2_3() {
+        assertEquals(Arrays.asList(2, 3), Factor.getFactors(6));
+    }
+
+    @Test
+    public void getFactors_of_7_returns_7() {
+        assertEquals(Arrays.asList(7), Factor.getFactors(7));
+    }
+
 }

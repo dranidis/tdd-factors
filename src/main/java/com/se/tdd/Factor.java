fb7d6103f76a1f2d589b347ae3e27d90998c47cc
D Dranidis <dranidis@gmail.com>
2021-04-02 22:00:26 +0300

    Factors 1: tests pass

--- src/main/java/com/se/tdd/Factor.java
+++ src/main/java/com/se/tdd/Factor.java
@@ -1,11 +1,12 @@
 package com.se.tdd;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Factor {
 
     public static List<Integer> getFactors(int i) {
-        return null;
+        return new ArrayList<Integer>();
     }
 
 }
